4aab3e9fefe9df6c9577e7cdf080bd11539f1327
nikunj ladva <nikunjladva245@gmail.com>
2021-08-15 13:30:04 +0530

    envirmnet varibale add

--- todo_app/src/firebase.js
+++ todo_app/src/firebase.js
@@ -5,12 +5,12 @@ import firebase from "firebase";
 //   };
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyD7uAJPvayZaPK52iNDIM5_xYvLwzE81fs",
-    authDomain: "todo-app-917cc.firebaseapp.com",
-    projectId: "todo-app-917cc",
-    storageBucket: "todo-app-917cc.appspot.com",
-    messagingSenderId: "580782240288",
-    appId: "1:580782240288:web:6588e42c24c25e43f40245"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 const db = firebaseApp.firestore();
